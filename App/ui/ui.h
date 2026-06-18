e8bcb8c0abad9c9c093ee95194f49e66af69d981
liuhuaxijun <98688750+liuhuaxijun@users.noreply.github.com>
2026-06-18 16:27:04 +0800

    v4.0.3

--- App/ui/ui.h
+++ App/ui/ui.h
@@ -11,4 +11,11 @@ void ui_create(void);
 // 更新电池电量
 void ui_updateBattery(bool isCharge, float v);
 
+/**
+ * @brief 更新运动状态
+ *
+ * @param speed
+ */
+void ui_updateBikeState(float speed);
+
 #endif
